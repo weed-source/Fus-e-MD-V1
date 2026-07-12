dbfb669ac5a3a42cc08ffb5738ba49a001cf2059
weed-source <dasoodeborah@gmail.com>
2026-07-12 14:32:15 -0400

    Add files via upload

---
+++ plugins/owner.js
@@ -0,0 +1,63 @@
+const { cmd } = require('../command');
+const config = require('../config');
+
+cmd({
+    pattern: "owner",
+    react: "✅", 
+    desc: "Get owner number",
+    category: "main",
+    filename: __filename
+}, 
+async (conn, mek, m, { from }) => {
+    try {
+        const ownerNumber = config.OWNER_NUMBER; // Fetch owner number from config
+        const ownerName = config.OWNER_NAME;     // Fetch owner name from config
+
+        const vcard = 'BEGIN:VCARD\n' +
+                      'VERSION:3.0\n' +
+                      `FN:Gotar Tech\n` +  
+                      `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` + 
+                      'END:VCARD';
+
+        // Send the vCard
+        const sentVCard = await conn.sendMessage(from, {
+            contacts: {
+                displayName: ownerName,
+                contacts: [{ vcard }]
+            }
+        });
+
+        // Send the owner contact message with image and audio
+        await conn.sendMessage(from, {
+            image: { url: 'https://files.catbox.moe/hcfwl1.jpg ' }, // Image URL from your request
+            caption: `╭━━〔 FUSÉE-MD-OWNER 〕━━┈⊷
+┃◈╭─────────────·๏
+┃◈┃• *Here is the owner details*
+┃◈┃• *Name* - ${ownerName}
+┃◈┃• *Number* ${ownerNumber}
+┃◈┃• *Version*: 1.0.0 Beta
+┃◈└───────────┈⊷
+╰──────────────┈⊷
+> *MADE IN BY WEED-TECH💡💫*`, // Display the owner's details
+            contextInfo: {
+                mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+ 
+                    newsletterName: 'Fusée MD,
+                    serverMessageId: 143
+                }            
+            }
+        }, { quoted: mek });
+
+    } catch (error) {
+        console.error(error);
+        reply(`An error occurred: ${error.message}`);
+    }
+});
+   } catch (error) {
+        console.error(error);
+        reply(`An error occurred: ${error.message}`);
+    }
+});
